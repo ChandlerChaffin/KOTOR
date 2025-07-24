77de7212cee8350739940d72505c11a2d18dce48
ChandlerChaffin <chandlerchaffin01@gmail.com>
2025-07-24 18:32:31 -0400

    Added combat menu

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ Array.prototype.sample = function(){
 	return this[Math.floor(Math.random()*this.length)];
 }
 class PlayerCharacter {
-	constructor(name, strength, dexterity, intelligence, constitution, wisdom, charisma) {
+	constructor(name, strength, dexterity, intelligence, constitution, wisdom, charisma, dead_bool) {
 		this.name = name;
 		this.strength = strength;
 		this.dexterity = dexterity;
@@ -14,6 +14,7 @@ class PlayerCharacter {
 		this.currentHealth = this.maxHealth;
 		this.maxMana = 100;
 		this.currentMana = this.maxMana;
+		this.isDead = dead_bool;
 	}
 	getModifier(stat) {
 		return Math.floor((stat-10)/2);
@@ -36,8 +37,8 @@ class PlayerCharacter {
 		const damage = Math.floor(Math.random()*6) + 1 + this.getModifier(this.strength);
 		console.log(`${this.name} attacks ${target.name} with a roll of ${attackRoll}.`);
 		if (attackRoll >= 12) {
-			target.takeDamage(damage);
 			console.log(`${this.name} deals ${damage} to ${target.name}.`);
+			target.takeDamage(damage);
 		}
 		else {
 			console.log(`${this.name} misses!`);
@@ -45,7 +46,7 @@ class PlayerCharacter {
 	}
 }
 class Nonplayer_Character {
-	constructor(name, strength, dexterity, intelligence, constitution, wisdom, charisma) {
+	constructor(name, strength, dexterity, intelligence, constitution, wisdom, charisma, dead_bool) {
 		this.name = name;
 		this.strength = strength;
 		this.dexterity = dexterity;
@@ -53,10 +54,11 @@ class Nonplayer_Character {
 		this.constitution = constitution; 
 		this.wisdom = wisdom;
 		this.charisma = charisma;
-		this.maxHealth = 100;
+		this.maxHealth = 50;
 		this.currentHealth = this.maxHealth;
 		this.maxMana = 100;
 		this.currentMana = this.maxMana;
+		this.isDead = dead_bool;
 	}
 	getModifier(stat) {
 		return Math.floor((stat-10)/2);
@@ -79,8 +81,8 @@ class Nonplayer_Character {
 		const damage = Math.floor(Math.random()*6) + 1 + this.getModifier(this.strength);
 		console.log(`${this.name} attacks ${target.name} with a roll of ${attackRoll}.`);
 		if (attackRoll >= 12) {
-			target.takeDamage(damage);
 			console.log(`${this.name} deals ${damage} to ${target.name}.`);
+			target.takeDamage(damage);
 		}
 		else {
 			console.log(`${this.name} misses!`);
@@ -97,31 +99,30 @@ const last_names = ['Tane', 'Bibble', 'Harand', 'Shule', 'Zapal', 'Horne', 'Vane
 
 function start_game() { 
 	let intro = "Welcome to Star Wars Knights of the Old Republic \n" 
-	let menu = " 1. New Game \n 2. Load Game \n 3. Exit \n"
+	let menu = "1. New Game \n2. Load Game \n3. Exit \n"
 	console.log(intro);
 	console.log(menu);
 	let choice = prompt("Enter an integer to proceed:");
 	if (choice == 1) {
-		let character_menu = " Character Creation Options\n 1. Custom \n 2. Quick \n"
+		let character_menu = "\nCharacter Creation Options\n 1. Custom \n 2. Quick \n"
 		console.log(character_menu);
 		let character_choice = prompt("Enter an integer to proceed:")
 		if (character_choice == 1) { 
 			let character_name= prompt("Enter your character name: "); 
-			console.log("\n");
-			console.log(`Choose your class: \n 1. Soldier \n 2. Scout \n 3. Scoundrel \n`);
+			console.log(`\nChoose your class: \n 1. Soldier \n 2. Scout \n 3. Scoundrel \n`);
 			var choice_class = Number(prompt(``));
 			if (choice_class == 1) {
-				const Main = new PlayerCharacter(character_name,17,14,12,16,10,8);
+				const Main = new PlayerCharacter(character_name,17,14,12,16,10,8,false);
 				console.log(`\nYou are ${Main.name}, a former Republic soldier. You find yourself alone on the floor of a medical bay \nwith little recollection of how you got there.`);
 				return Main;
 			}
 			else if (choice_class == 2) {
-				const Main = new PlayerCharacter(character_name, 14, 17, 16, 10, 15, 17);
+				const Main = new PlayerCharacter(character_name, 14, 17, 16, 10, 15, 17,false);
 				console.log(`\nYou are ${Main.name}, a scout for an underground smuggling buisiness. You find yourself alone on the floor of a medical bay \nwith little recollection of how you got there.`);
 				return Main;
 			}
 			else if (choice_class == 3){
-				const Main = new PlayerCharacter(character_name, 12,18,14,10,15,18);
+				const Main = new PlayerCharacter(character_name, 12,18,14,10,15,18,false);
 				console.log(`\nYou are ${Main.name}, a scoundrel whose only loyalty is to galactic credits. You find yourself alone on the floor of a medical bay \nwith little recollection of how you got there.`);
 				return Main;
 			}
@@ -132,8 +133,8 @@ function start_game() {
 	}
 		else if (character_choice == 2) {
 			let character_name = first_names.sample() + " " + last_names.sample();
-			const Main = new PlayerCharacter(character_name, 16, 12, 14, 13, 10, 8);
-			console.log(`Choose your class:\n 1. Soldier \n 2. Scout \n 3. Scoundrel\n`);
+			const Main = new PlayerCharacter(character_name, 16, 12, 14, 13, 10, 8,false);
+			console.log(`\nChoose your class:\n 1. Soldier \n 2. Scout \n 3. Scoundrel\n`);
 			var choice_class = Number(prompt(``));
 			if (choice_class == 1) {
 				console.log(`\nYou are ${Main.name}, a former Republic soldier. You find yourself alone on the floor of a medical bay \nwith little recollection of how you got there.`);
@@ -151,6 +152,10 @@ function start_game() {
 				console.log('Error. Game exiting..');
 				return null;
 			}
+		}
+		else { 
+			console.log('Error. Game exiting..');
+			return null;
 		}
 	}
 
@@ -173,26 +178,44 @@ function fight_menu(pc) {
 	console.log(`\nChoose your opponent. \n1. Darth Sion \n2. Darth Nihilus \n3. Darth Treya \n`);
 	fight_choice = new Number(prompt(``));
 	if (fight_choice == 1) { 
-		console.log(red + 'The call of Korriban is strong but is it the call of the dead. It is fitting that you came here.\n'+ reset);
-		const Sion = new Nonplayer_Character('Darth Sion', 16,16,16,16,16.16); 
+		console.log(red + '\nThe call of Korriban is strong but is it the call of the dead. It is fitting that you came here.\n'+ reset);
+		const Sion = new Nonplayer_Character('Darth Sion', 16,16,16,16,16,16,false); 
 		return Sion;
 	}
 	else if (fight_choice == 2){
-		console.log(red + 'I am the darkness in which all life  dies' + reset);
-		const Nihilus = new Nonplayer_Character('Darth Nihilus', 15, 15, 15, 18, 18, 18);
+		console.log(red + '\nI am the darkness in which all life  dies' + reset);
+		const Nihilus = new Nonplayer_Character('Darth Nihilus', 15, 15, 15, 18, 18, 18,false);
 		return Nihilus; 
 	}
 	else if (fight_choice == 3) {
-		console.log(green + `At last you have arrived.. is Malachor as you remember? You no doubt have many questions.\n I would be a poor teacher if I did not give you the answers you seek here.` + reset);
-		const Treya = new Nonplayer_Character('Darth Treya', 14, 14, 19, 20, 20, 20);
+		console.log(green + `\nAt last you have arrived.. is Malachor as you remember? You no doubt have many questions.\nI would be a poor teacher if I did not give you the answers you seek here.` + reset);
+		const Treya = new Nonplayer_Character('Darth Treya', 14, 14, 19, 20, 20, 20,false);
 		return Treya; 
 	}
 	else { 
-		console.log('Input Error... Exiting');
+		console.log('\nInput Error... Exiting');
 		return null; 
 	}
 }
 
 mycharacter = start_game();
+if (mycharacter != null) {
 enemy = fight_menu(mycharacter);
-
+while (mycharacter.isDead == false) {
+console.log("\nAction Menu\n1.Attack\n");
+action_choice = prompt('');
+if (action_choice == 1) {
+	mycharacter.attack(enemy);
+	if(enemy.isDead == false) {
+	enemy.attack(mycharacter);
+	}
+	else {
+	console.log(green +'You Win!'+reset);
+	break;
+	}
+}
+else {
+	console.log(red + 'You fail.'+reset);
+}
+}
+}
